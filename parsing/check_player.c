44b94a3fa2669e30124605246b4db1ca310c92c2
Homma Rinka <hrinka@c2r2s1.42tokyo.jp>
2024-06-18 22:58:14 +0900

    separate get_player_pos

--- parsing/check_player.c
+++ parsing/check_player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_player.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hirosuzu <hirosuzu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hrinka <hrinka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 19:51:53 by hrinka            #+#    #+#             */
-/*   Updated: 2024/06/18 00:09:56 by hirosuzu         ###   ########.fr       */
+/*   Updated: 2024/06/18 22:57:48 by hrinka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,31 +42,36 @@ void	duplicate_player(t_cub3d *data)
 	}
 }
 
+int	check_player_pos(t_cub3d *data, int i, int j)
+{
+	if (data->map.map[i][j] == 'N' || data->map.map[i][j] == 'S' \
+		|| data->map.map[i][j] == 'E' || data->map.map[i][j] == 'W')
+	{
+		data->player.i = i;
+		data->player.j = j;
+		data->player.pos_x = j + 0.5;
+		data->player.pos_y = i + 0.5;
+		data->player.direction = data->map.map[i][j];
+		return (0);
+	}
+	return (1);
+}
+
 void	get_player_pos(t_cub3d *data)
 {
 	int	i;
 	int	j;
 
-	i = 0;
-	printf("start get_player_pos\n");
+	i = 1;
 	while (i < data->map.height_map)
 	{
 		j = 0;
 		data->map.map[i] = ft_realloc(data->map.map[i], \
-							sizeof(char) * ft_strlen(data->map.map[i]), data->map.width_map + 1);
+		sizeof(char) * ft_strlen(data->map.map[i]), data->map.width_map + 1);
 		while (j < data->map.width_map)
 		{
-			printf("data->map.map[%d][%d] = %c\n", i, j, data->map.map[i][j]);
-			if (data->map.map[i][j] == 'N' || data->map.map[i][j] == 'S' \
-			|| data->map.map[i][j] == 'E' || data->map.map[i][j] == 'W')
-			{
-				data->player.i = i;
-				data->player.j = j;
-				data->player.pos_x = j + 0.5;
-				data->player.pos_y = i + 0.5;
-				data->player.direction = data->map.map[i][j];
+			if (check_player_pos(data, i, j) == 0)
 				return ;
-			}
 			j++;
 		}
 		i++;
